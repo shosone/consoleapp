843c0c0db0532a39e8f9615703c2520464993e39
yumetodo <yume-wikijp@live.jp>
2018-10-28 23:24:29 +0900

    fix(cmake): follow directry change
    cut out checking termious api logic to macro

---
+++ cmake/modules/CheckTermiosAPI.cmake
@@ -0,0 +1,32 @@
+﻿#[=======================================================================[.rst:
+CheckIncludeFile
+----------------
+
+Provides a macro to check if a header file can be included in ``C``.
+
+.. command:: CHECK_TERMIOS_API
+
+  ::
+
+    CHECK_TERMIOS_API(<variable>)
+
+  Check termios api and store the result in an internal cache entry named
+  ``<variable>``.
+
+#]=======================================================================]
+
+include_guard(GLOBAL)
+
+macro(CHECK_TERMIOS_API VARIABLE)
+  if(MSVC)
+    set(${VARIABLE} "" CACHE INTERNAL "Have Termios API")
+  else()
+    include(CheckIncludeFile)
+    CHECK_INCLUDE_FILE(termios.h ${VARIABLE})
+    if(${VARIABLE})
+      set(${VARIABLE} 1 CACHE INTERNAL "Have Termios API")
+    else()
+      set(${VARIABLE} "" CACHE INTERNAL "Have Termios API")
+    endif()
+  endif()
+endmacro()
